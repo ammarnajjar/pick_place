ae3d6c3209adc14a83b5396d36a45069ce409c12
Ammar Najjar <najjarammar@gmail.com>
2016-09-14 13:29:28 +0200

    Introduce Pose class and move_robot_to function.

--- src/pick_place.cpp
+++ src/pick_place.cpp
@@ -25,15 +25,31 @@
 #include <kinect2_viewer/Vec_Obj.h>
 //#include <moveit/robot_state/
 
-double dXObj;
-double dYObj;
-
-double dXGripper;
-double dYGripper;
-double dZGripper;
-
+// Globals
 ros::Subscriber sub;
-int aux;
+int aux = -1;
+std::vector<kinect2_viewer::Merosy_Obj> detected_objects;
+std::vector<kinect2_viewer::Merosy_Obj> manipulator;
+
+
+class Pose {
+	public:
+		Pose(double x, double y, double z, double roll, double pitch, double yaw) {
+			dX = x;
+			dY = y;
+			dZ = z;
+			roll_deg = roll;
+			pitch_deg = pitch;
+			yaw_deg = yaw;
+		}
+		double dX;
+		double dY;
+		double dZ;
+		double roll_deg;
+		double pitch_deg;
+		double yaw_deg;
+};
+
 
 void send_joints_to_robot(const std::vector<std::string>& joint_names_received, std::vector<double>& joint_values_received, robot_state::RobotState &current_values)
 {
@@ -84,15 +100,6 @@ void send_joints_to_robot(const std::vector<std::string>& joint_names_received, 
 
 }
 
-void send_home(const std::vector<std::string> &joint_names, robot_state::RobotState &current_values)
-{
-    std::vector<double> joint_values;
-	for (size_t i = 0; i < joint_values.size(); ++i) {
-		joint_values.push_back(0.0);
-	}
-    send_joints_to_robot(joint_names, joint_values, current_values);
-}
-
 bool isStateValid(const planning_scene::PlanningScene *planning_scene, robot_state::RobotState *state,
                   const robot_state::JointModelGroup *group, const double *joint_group_variable_values)
 {
@@ -121,10 +128,20 @@ bool isStateValid(const planning_scene::PlanningScene *planning_scene, robot_sta
     return planning_scene->isStateFeasible(*state);
 }
 
+kinect2_viewer::Merosy_Obj get_obj_closest_to_camera(
+		std::vector<kinect2_viewer::Merosy_Obj>& objs) {
+	double closest = 100;
+	for (size_t i = 0; i < objs.size(); ++i) {
+		if (objs.at(i).center_x < closest) {
+			closest = objs.at(i).center_x;
+		}
+	}
+	return objs.at(closest);
+}
+
+
 void objCallback(const kinect2_viewer::Vec_Obj::ConstPtr& msg)
 {
-	std::vector<kinect2_viewer::Merosy_Obj> detected_objects;
-
 	// no detexted objects
     if (msg->objs.size() <= 1)
     {
@@ -133,15 +150,12 @@ void objCallback(const kinect2_viewer::Vec_Obj::ConstPtr& msg)
     }
 
 	// gripper and many objects
-    else if (msg->objs.size() > 1) {
+    else {
 		for (size_t i = 0; i < msg->objs.size(); ++i) {
-			if (msg->objs[i].center_z < 0.1) {
+			if (msg->objs[i].min_z < 0.1) {
 				detected_objects.push_back(msg->objs[i]);
 			} else {
-				kinect2_viewer::Merosy_Obj gripper = msg->objs[i];
-				dXGripper = gripper.center_x;
-				dYGripper = gripper.center_y;
-				dZGripper = gripper.center_z;
+				manipulator.push_back(msg->objs[i]);
 			}
 		}
 	}
@@ -156,20 +170,84 @@ void objCallback(const kinect2_viewer::Vec_Obj::ConstPtr& msg)
 
 	std::cout << "To where do you want to move? (Press 0 to Cancel)" << std::endl;
     std::cin>>aux;
+}
 
-	std::cout << "Moving to Object " << aux << std::endl;
+Eigen::Affine3d get_obj_pose(
+		double dXObj,double dYObj, double dZObj,
+		double roll_deg,double pitch_deg,double yaw_deg)
+{
+		double roll_rad = roll_deg * (M_PI/180);
+		double pitch_rad = pitch_deg * (M_PI/180);
+		double yaw_rad = yaw_deg * (M_PI/180);
 
-	if (aux > 0 and aux <= (int)detected_objects.size()) {
-		dXObj = detected_objects.at(aux-1).center_x;
-		dYObj = detected_objects.at(aux-1).center_y;
+		tf::Quaternion l_q;
+		l_q.setRPY(roll_rad, pitch_rad, yaw_rad);
+
+		double dXCenter = 0.64;
+		double dYCenter = -0.015;
+		double dZCenter = 0.185;
+
+		/* Arm position */
+		Eigen::Affine3d goal_pose = Eigen::Translation3d(dXCenter-dXObj, dYCenter-dYObj, dZCenter-dZObj) * Eigen::Quaterniond(l_q);
+		return goal_pose;
+}
+
+void move_robot_to(Pose& pose)
+{
+	moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+
+	// wait a bit for ros things to initialize
+	ros::WallDuration(1.0).sleep();
+
+	/* Model, scene and state */
+	robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
+	robot_model::RobotModelPtr kinematic_model = robot_model_loader.getModel();
+	planning_scene::PlanningScene planning_scene(kinematic_model);
+	robot_state::RobotStatePtr kinematic_state(new robot_state::RobotState(kinematic_model));
+
+	/* Joints and Robot group */
+	std::vector<double> joint_values;
+	const robot_state::JointModelGroup* joint_model_group = kinematic_model->getJointModelGroup("manipulator");
+	const std::vector<std::string> &joint_names = joint_model_group->getJointModelNames();
+
+	/* Get the current state */
+	planning_scene_monitor::PlanningSceneMonitor psm("robot_description");
+	psm.startStateMonitor();
+	sleep(1);
+	robot_state::RobotState current_values = psm.getPlanningScene()->getCurrentState();
+	psm.stopStateMonitor();
+
+	/* Put the kinematic state in current state */
+	current_values.copyJointGroupPositions(joint_model_group, joint_values);
+	kinematic_state->setJointGroupPositions(joint_model_group, joint_values);
+
+
+	/* Print joint names and values */
+	for(std::size_t i = 0; i < joint_names.size(); ++i)
+	{
+		ROS_INFO("Joint %s: %f", joint_names[i].c_str(), joint_values[i]);
 	}
 
 
-//    for (int i=0;i<msg->objs.size();i++)
-//    {
-//        kinect2_viewer::Merosy_Obj obj = msg->objs[i];
-//        ROS_INFO("I heard: [%d]", obj.id);
-//    }
+	// Move the arm in position
+	Eigen::Affine3d goal_pose = get_obj_pose(pose.dX, pose.dY, pose.dZ, pose.roll_deg, pose.pitch_deg, pose.yaw_deg);
+
+	/* Arm group, model and end-effector */
+	moveit::planning_interface::MoveGroup group("manipulator");
+	std::string end_effector_name = group.getEndEffectorLink();
+
+	/* Arm IK */
+	if (!kinematic_state->setFromIK(joint_model_group, goal_pose, end_effector_name, 200, 0.1,
+				boost::bind(&isStateValid, &planning_scene, _1, _2, _3)))
+	{
+		ROS_WARN("Was NOT able to set IK");
+		return;
+	}
+
+	kinematic_state->copyJointGroupPositions(joint_model_group, joint_values);
+
+	send_joints_to_robot(joint_names, joint_values, current_values);
+	sleep(5);
 }
 
 
@@ -179,130 +257,48 @@ int main(int argc, char **argv)
     ros::AsyncSpinner spinner(1);
     spinner.start();
 
-    dXObj = 0;
-    dYObj = 0;
-
-    aux = 0;
-
-    ros::NodeHandle nh;
-
-    sub = nh.subscribe("/merosy_objects", 5, objCallback);
-
-    while (aux < 1)
-        sleep(1);
-
-    moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
-
-    // wait a bit for ros things to initialize
-    ros::WallDuration(1.0).sleep();
-
-    /* Model, scene and state */
-    robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
-    robot_model::RobotModelPtr kinematic_model = robot_model_loader.getModel();
-    planning_scene::PlanningScene planning_scene(kinematic_model);
-    robot_state::RobotStatePtr kinematic_state(new robot_state::RobotState(kinematic_model));
-
-    /* Joints and Robot group */
-    std::vector<double> joint_values;
-    const robot_state::JointModelGroup* joint_model_group = kinematic_model->getJointModelGroup("manipulator");
-    const std::vector<std::string> &joint_names = joint_model_group->getJointModelNames();
-
-    /* Get the current state */
-    planning_scene_monitor::PlanningSceneMonitor psm("robot_description");
-    psm.startStateMonitor();
-    sleep(1);
-    robot_state::RobotState current_values = psm.getPlanningScene()->getCurrentState();
-    psm.stopStateMonitor();
-
-    /* Put the kinematic state in current state */
-    current_values.copyJointGroupPositions(joint_model_group, joint_values);
-    kinematic_state->setJointGroupPositions(joint_model_group, joint_values);
-
-
-    /* Print joint names and values */
-    for(std::size_t i = 0; i < joint_names.size(); ++i)
-    {
-        ROS_INFO("Joint %s: %f", joint_names[i].c_str(), joint_values[i]);
-    }
-
-
-    // Move the arm in position
-    double l_roll_deg=0;
-    double l_pitch_deg=180;
-    double l_yaw_deg=-90;
-
-    double l_roll_rad = l_roll_deg * (M_PI/180);
-    double l_pitch_rad = l_pitch_deg * (M_PI/180);
-    double l_yaw_rad = l_yaw_deg * (M_PI/180);
-
-    tf::Quaternion createQuaternionFromRPY(double l_roll_rad,double l_pitch_rad,double l_yaw_rad);
-    tf::Quaternion l_q;
-    l_q.setRPY(l_roll_rad, l_pitch_rad, l_yaw_rad);
-
-
-    double dXCenter = 0.64;
-    double dYCenter = -0.015;
-    double dZCenter = 0.185;
-
-    /* Arm position */
-    Eigen::Affine3d goal_pose = Eigen::Translation3d(dXCenter-dXObj, dYCenter-dYObj, dZCenter) //Z + Height/2 + error
-            * Eigen::Quaterniond(l_q);
-
-    /* Arm group, model and end-effector */
-    moveit::planning_interface::MoveGroup group("manipulator");
-    std::string end_effector_name = group.getEndEffectorLink();
-
-
-    /* Arm IK */
-    if (!kinematic_state->setFromIK(joint_model_group, goal_pose, end_effector_name, 200, 0.1,
-                                    boost::bind(&isStateValid, &planning_scene, _1, _2, _3)))
-    {
-        ROS_WARN("Was NOT able to set IK for ");
-        return false;
-    }
-
-    kinematic_state->copyJointGroupPositions(joint_model_group, joint_values);
-
-    send_joints_to_robot(joint_names, joint_values, current_values);
-    sleep(5);
+	double dXObj = 0;
+	double dYObj = 0;
+	double dZObj = 0;
+	double dXGripper = 0.25;
+	double dYGripper = -0.0003;
+	double dZGripper = 0.441;
 
 	{
-		/* Get the current state */
-		psm.startStateMonitor();
-		sleep(1);
-		current_values = psm.getPlanningScene()->getCurrentState();
-		psm.stopStateMonitor();
+		ros::NodeHandle nh;
+		sub = nh.subscribe("/merosy_objects", 5, objCallback);
 
-		// Move the arm in position
-		l_roll_deg=-45;
-		l_pitch_deg=-180;
-		l_yaw_deg=-90;
+		while (aux < 1)
+			sleep(1);
 
-		l_roll_rad = l_roll_deg * (M_PI/180);
-		l_pitch_rad = l_pitch_deg * (M_PI/180);
-		l_yaw_rad = l_yaw_deg * (M_PI/180);
-
-		l_q.setRPY(l_roll_rad, l_pitch_rad, l_yaw_rad);
-
-		/* Put the kinematic state in current state */
-		current_values.copyJointGroupPositions(joint_model_group, joint_values);
-		kinematic_state->setJointGroupPositions(joint_model_group, joint_values);
-
-		goal_pose = Eigen::Translation3d(dXGripper+0.2, dYGripper, dZGripper + 0.1) * Eigen::Quaterniond(l_q);
-
-		if (!kinematic_state->setFromIK(joint_model_group, goal_pose, end_effector_name, 200, 0.1,
-					boost::bind(&isStateValid, &planning_scene, _1, _2, _3)))
-		{
-			ROS_WARN("Was NOT able to set IK for ");
-			return false;
+		if (aux > 0 and aux <= (int)detected_objects.size()) {
+			std::cout << "Moving to Object " << aux << std::endl;
+			dXObj = detected_objects.at(aux-1).center_x;
+			dYObj = detected_objects.at(aux-1).center_y;
+			dZObj = detected_objects.at(aux-1).center_z;
 		}
 
-		kinematic_state->copyJointGroupPositions(joint_model_group, joint_values);
+		// if (manipulator.size() > 0) {
+		// 	kinect2_viewer::Merosy_Obj gripper = get_obj_closest_to_camera(manipulator);
+		// 	dXGripper = gripper.center_x;
+		// 	dYGripper = gripper.center_y;
+		// 	dZGripper = gripper.center_z;
+		// }
 
-		send_joints_to_robot(joint_names, joint_values, current_values);
-		sleep(5);
+		// Move the arm in position
+		double roll_deg=0;
+		double pitch_deg=180;
+		double yaw_deg=-90;
+		Pose pose1 = Pose(dXObj, dYObj, dZObj-0.02, roll_deg, pitch_deg, yaw_deg);
+		move_robot_to(pose1);
+
+		{
+			// Move to preset active position
+			Pose pose = Pose(dXGripper, dYGripper, dZGripper-0.73, -45, -180, -90);
+			move_robot_to(pose);
+		}
+
 	}
-
     ros::shutdown();
     return 0;
 }
