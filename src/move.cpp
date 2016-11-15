8476f00b397da4d969b9839648b0660b63f19fb6
Ammar Najjar <najjarammar@gmail.com>
2016-11-15 13:45:43 +0100

    improve indentation

--- src/move.cpp
+++ src/move.cpp
@@ -57,70 +57,70 @@ void send_joints_to_robot(const std::vector<std::string>& joint_names_received,
 		std::vector<double>& joint_values_received, robot_state::RobotState &current_values)
 {
 
-    /* Move group for Robot and joints set */
-    moveit::planning_interface::MoveGroup group("manipulator");
+	/* Move group for Robot and joints set */
+	moveit::planning_interface::MoveGroup group("manipulator");
 
-    group.setPlannerId("RRTkConfigDefault");
-    std::map<std::string, double> joints;
+	group.setPlannerId("RRTkConfigDefault");
+	std::map<std::string, double> joints;
 
-    for(std::size_t i = 0; i < joint_names_received.size(); ++i)
-    {
-        joints[joint_names_received[i].c_str()] = joint_values_received[i];
-        //ROS_INFO("Joints r %s: %f", joint_names_received[i].c_str(), joint_values_received[i]);
-    }
+	for(std::size_t i = 0; i < joint_names_received.size(); ++i)
+	{
+		joints[joint_names_received[i].c_str()] = joint_values_received[i];
+		//ROS_INFO("Joints r %s: %f", joint_names_received[i].c_str(), joint_values_received[i]);
+	}
 
-    // New current state
-    planning_scene_monitor::PlanningSceneMonitorPtr final_planning_scene =
-            boost::make_shared<planning_scene_monitor::PlanningSceneMonitor>("robot_description");
-    group.setStartState(current_values);
+	// New current state
+	planning_scene_monitor::PlanningSceneMonitorPtr final_planning_scene =
+		boost::make_shared<planning_scene_monitor::PlanningSceneMonitor>("robot_description");
+	group.setStartState(current_values);
 
-    group.setJointValueTarget(joints);
+	group.setJointValueTarget(joints);
 
-    moveit::planning_interface::MoveGroup::Plan plan;
-    if (!group.plan(plan))
-    {     //If fails we try with PRMkConfigDefault
-        group.setPlannerId("PRMkConfigDefault");
-        group.setStartState(current_values);
-        group.setJointValueTarget(joints);
+	moveit::planning_interface::MoveGroup::Plan plan;
+	if (!group.plan(plan))
+	{     //If fails we try with PRMkConfigDefault
+		group.setPlannerId("PRMkConfigDefault");
+		group.setStartState(current_values);
+		group.setJointValueTarget(joints);
 
-        if (!group.plan(plan))
-            ROS_FATAL("Unable to create motion plan.  Aborting.");
-        else
-        {
-            std::cout << "Move the robot? (Press 1 if YES)";
-            aux = 1;
-            if(aux==1)
-                group.execute(plan);
-        }
-    }
-    else
-    {
-        std::cout << "Move the robot? (Press 1 if YES)";
-        aux = 1;
-        if(aux==1)
-            group.execute(plan);
-    }
+		if (!group.plan(plan))
+			ROS_FATAL("Unable to create motion plan.  Aborting.");
+		else
+		{
+			std::cout << "Move the robot? (Press 1 if YES)";
+			aux = 1;
+			if(aux==1)
+				group.execute(plan);
+		}
+	}
+	else
+	{
+		std::cout << "Move the robot? (Press 1 if YES)";
+		aux = 1;
+		if(aux==1)
+			group.execute(plan);
+	}
 }
 
 bool isStateValid(const planning_scene::PlanningScene *planning_scene, robot_state::RobotState *state,
-                  const robot_state::JointModelGroup *group, const double *joint_group_variable_values)
+		const robot_state::JointModelGroup *group, const double *joint_group_variable_values)
 {
-    state->setJointGroupPositions(group, joint_group_variable_values);
-    std::vector<double> joint_values;
-    state->copyJointGroupPositions(group, joint_values);
-    collision_detection::CollisionRequest request;
-    request.verbose = false;
-    request.group_name = group->getName();
-    collision_detection::CollisionResult result;
-    planning_scene->checkCollision(request, result, *state);
+	state->setJointGroupPositions(group, joint_group_variable_values);
+	std::vector<double> joint_values;
+	state->copyJointGroupPositions(group, joint_values);
+	collision_detection::CollisionRequest request;
+	request.verbose = false;
+	request.group_name = group->getName();
+	collision_detection::CollisionResult result;
+	planning_scene->checkCollision(request, result, *state);
 
-    if (result.collision)
-        return false;
-    // limit for joints for the IK validation to be above the table
-    else if (joint_values[1] > 0.3 || joint_values[0] < -0.5  || joint_values[4] < 0.3 || joint_values[4] > 1.5)
-       return false;
+	if (result.collision)
+		return false;
+	// limit for joints for the IK validation to be above the table
+	else if (joint_values[1] > 0.3 || joint_values[0] < -0.5  || joint_values[4] < 0.3 || joint_values[4] > 1.5)
+		return false;
 
-    return planning_scene->isStateFeasible(*state);
+	return planning_scene->isStateFeasible(*state);
 }
 
 kinect2_viewer::Merosy_Obj get_obj_closest_to_camera(
@@ -138,14 +138,14 @@ kinect2_viewer::Merosy_Obj get_obj_closest_to_camera(
 void objCallback(const kinect2_viewer::Vec_Obj::ConstPtr& msg)
 {
 	// no detexted objects
-    if (msg->objs.size() <= 1)
-    {
-        ROS_ERROR("Please put at least one object on the table");
-        return;
-    }
+	if (msg->objs.size() <= 1)
+	{
+		ROS_ERROR("Please put at least one object on the table");
+		return;
+	}
 
 	// gripper and many objects
-    else {
+	else {
 		for (size_t i = 0; i < msg->objs.size(); ++i) {
 			if (msg->objs[i].min_z < 0.1) {
 				detected_objects.push_back(msg->objs[i]);
@@ -155,13 +155,13 @@ void objCallback(const kinect2_viewer::Vec_Obj::ConstPtr& msg)
 		}
 	}
 
-    sub.shutdown();
+	sub.shutdown();
 
 	for (size_t i = 0; i < detected_objects.size(); ++i) {
-    std::cout << "[" << i+1 << "]- Found object at (" <<
-		detected_objects.at(i).center_x << ", " <<
-		detected_objects.at(i).center_y << "," <<
-		detected_objects.at(i).center_z << ")" << std::endl;
+		std::cout << "[" << i+1 << "]- Found object at (" <<
+			detected_objects.at(i).center_x << ", " <<
+			detected_objects.at(i).center_y << "," <<
+			detected_objects.at(i).center_z << ")" << std::endl;
 	}
 
 	std::cout << "To where do you want to move? (Press 0 to Cancel)" << std::endl;
@@ -172,20 +172,20 @@ Eigen::Affine3d get_obj_pose(
 		double dXObj,double dYObj, double dZObj,
 		double roll_deg,double pitch_deg,double yaw_deg)
 {
-		double roll_rad = roll_deg * (M_PI/180);
-		double pitch_rad = pitch_deg * (M_PI/180);
-		double yaw_rad = yaw_deg * (M_PI/180);
+	double roll_rad = roll_deg * (M_PI/180);
+	double pitch_rad = pitch_deg * (M_PI/180);
+	double yaw_rad = yaw_deg * (M_PI/180);
 
-		tf::Quaternion l_q;
-		l_q.setRPY(roll_rad, pitch_rad, yaw_rad);
+	tf::Quaternion l_q;
+	l_q.setRPY(roll_rad, pitch_rad, yaw_rad);
 
-		double dXCenter = 0.64;
-		double dYCenter = -0.015;
-		double dZCenter = 0.185;
+	double dXCenter = 0.64;
+	double dYCenter = -0.015;
+	double dZCenter = 0.185;
 
-		/* Arm position */
-		Eigen::Affine3d goal_pose = Eigen::Translation3d(dXCenter-dXObj, dYCenter-dYObj, dZCenter-dZObj) * Eigen::Quaterniond(l_q);
-		return goal_pose;
+	/* Arm position */
+	Eigen::Affine3d goal_pose = Eigen::Translation3d(dXCenter-dXObj, dYCenter-dYObj, dZCenter-dZObj) * Eigen::Quaterniond(l_q);
+	return goal_pose;
 }
 
 void move_robot_to(Pose& pose)
@@ -246,20 +246,20 @@ void move_robot_to(Pose& pose)
 
 int main(int argc, char **argv)
 {
-    ros::init (argc, argv, "move");
-    ros::AsyncSpinner spinner(1);
-    spinner.start();
+	ros::init (argc, argv, "move");
+	ros::AsyncSpinner spinner(1);
+	spinner.start();
 
 	std::cout << "argc = " << argc << std::endl;
 	double dXGripper = 0.25;
 	double dYGripper = -0.0003;
 	double dZGripper = 0.441;
-		double dXObj = 0;
-		double dYObj = 0;
-		double dZObj = 0;
-		double roll_deg=0;
-		double pitch_deg=180;
-		double yaw_deg=-90;
+	double dXObj = 0;
+	double dYObj = 0;
+	double dZObj = 0;
+	double roll_deg=0;
+	double pitch_deg=180;
+	double yaw_deg=-90;
 	if (argc >= 7) {
 		std::istringstream iss1( argv[1] );
 		iss1 >> dXObj;
@@ -275,12 +275,12 @@ int main(int argc, char **argv)
 		iss6 >> yaw_deg;
 	}
 
-		std::cout << "argv[1] = " << argv[1] << ", " << dXObj << std::endl;
-		std::cout << "argv[2] = " << argv[2] << ", " << dYObj << std::endl;
-		std::cout << "argv[3] = " << argv[3] << ", " << dZObj << std::endl;
-		std::cout << "argv[4] = " << argv[4] << ", " << roll_deg << std::endl;
-		std::cout << "argv[5] = " << argv[5] << ", " << pitch_deg << std::endl;
-		std::cout << "argv[6] = " << argv[6] << ", " << yaw_deg << std::endl;
+	std::cout << "argv[1] = " << argv[1] << ", " << dXObj << std::endl;
+	std::cout << "argv[2] = " << argv[2] << ", " << dYObj << std::endl;
+	std::cout << "argv[3] = " << argv[3] << ", " << dZObj << std::endl;
+	std::cout << "argv[4] = " << argv[4] << ", " << roll_deg << std::endl;
+	std::cout << "argv[5] = " << argv[5] << ", " << pitch_deg << std::endl;
+	std::cout << "argv[6] = " << argv[6] << ", " << yaw_deg << std::endl;
 	// Move the arm in position
 	Pose pose1 = Pose(dXObj, dYObj, dZObj-0.02, roll_deg, pitch_deg, yaw_deg);
 	move_robot_to(pose1);
@@ -294,7 +294,7 @@ int main(int argc, char **argv)
 		move_robot_to(pose);
 	}
 
-    ros::shutdown();
-    return 0;
+	ros::shutdown();
+	return 0;
 }
 
